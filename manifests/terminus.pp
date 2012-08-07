cdcb0df3b47c54d356440b43fd48c86e3e94b7bd
Stephen <stephen@puppetlabs.com>
2012-08-07 10:48:26 +0100

    remove all mention of puppetmaster and require the puppetmaster class

--- manifests/terminus.pp
+++ manifests/terminus.pp
@@ -5,22 +5,6 @@ class puppetdb::terminus(
         ensure => present,
     }
 
-    
-    #######Hack until i find a nice way to do this 
-    if ! defined(Package['puppetmaster'])
-    {
-        package { "puppetmaster":
-            ensure => present,
-        }
-    }
-
-    if ! defined(Service['puppetmaster'])
-    {
-        service { "puppetmaster":
-            ensure => running,
-        }
-    }
-
     # TODO: this will overwrite any existing routes.yaml;
     #  to handle this properly we should just be ensuring
     #  that the proper lines exist
@@ -28,7 +12,8 @@ class puppetdb::terminus(
     file { "$routes_file":
         ensure      => file,
         content     => template('puppetdb/terminus/routes.yaml.erb'),
-        notify      => Service['puppetmaster'],
+        require     => Class['puppet::master'],
+        notify      => $::puppet::master::service_notify
     }
 
     $notify_exec = "notify: puppet.conf changes required"
@@ -36,36 +21,8 @@ class puppetdb::terminus(
     file { "$puppetdb_conf_file":
         ensure      => file,
         content     => template('puppetdb/terminus/puppetdb.conf.erb'),
-        notify      => [Service['puppetmaster'], Exec[$notify_exec]]
+        notify      => $::puppet::master::service_notify
     }
-
-    # TODO: we also need to make some small changes to puppet.conf, but
-    #  that requires the ability to manipulate an .ini file, which
-    #  we can't easily accomplish at this point.  This exec is here
-    #  to notify the users of the situation.  Can't use a real 'notify'
-    #  because we only want this to appear on refresh.
-    $puppet_conf_changes_msg = "
-        Almost there!  To finish, you'll need to add the following lines to
-        your puppet.conf file, in the [main] section:
-
-            server=${settings::certname}
-            storeconfigs=true
-            storeconfigs_backend=puppetdb
-
-        Then you should be able to run 'puppet agent --test' to exercise
-        your puppetdb installation.
-
-        "
-    exec { $notify_exec:
-        refreshonly => true,
-        path        => '/bin:/usr/bin',
-        command     => "echo \"$puppet_conf_changes_msg\"",
-        logoutput   => true,
-    }
-
-    Package["puppetmaster"] ->
-        Package["puppetdb-terminus"] ->
-        File[$routes_file] ->
-        File[$puppetdb_conf_file] ->
-        Service["puppetmaster"]
+        
+    Package["puppetdb-terminus"] -> File[$routes_file] -> File[$puppetdb_conf_file] ->
 }
