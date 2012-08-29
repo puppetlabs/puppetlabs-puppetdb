80eba1a20ec7a3258a02a3382d654825f2548511
Stephen <stephen@puppetlabs.com>
2012-08-29 12:26:45 +0100

    cleanup, remove terminus as this is now in puppet module, use inifile

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,26 +1,95 @@
 class puppetdb::server(
-    $database = 'embedded',
-    $database_host = 'localhost',
-    $puppetdb_conf_dir = $puppetdb::params::confdir
+    $database              = 'embedded',
+    $psdatabase_host       = $puppetdb::params::psdatabase_host,
+    $psdatabase_port       = $puppetdb::params::psdatabase_port,
+    $psdatabase_username   = $puppetdb::params::psdatabase_username,
+    $psdatabase_password   = $puppetdb::params::psdatabase_password,  
+    $psdatabase            = $puppetdb::params::psdatabase_psdatabase,
+    $confdir               = $puppetdb::params::confdir,
+    $gc_interval           = $puppetdb::params::gc_interval
 ) inherits puppetdb::params {
 
     package { 'puppetdb':
-        ensure => present,
+        ensure  => present,
+        notifty => Service['puppetdb'],
     }
-
-    file { 'puppetdb: database.ini':
+    
+    file { "{confdir}/database.ini":
         ensure      => file,
-        path        => "${puppetdb_conf_dir}/database.ini",
-        content     => template('puppetdb/server/database.ini.erb'),
-        notify      => Service['puppetdb'],
+        path        => $puppetdb_ini_file,
+        require     => Package['puppetdb'],
     }
 
-    service { "puppetdb":
+    service { 'puppetdb':
         ensure => running,
-        #ensure => stopped,
-   }
+        require => File['puppetdb: database.ini'],
+    }
 
-    Package['puppetdb'] -> File['puppetdb: database.ini'] -> Service['puppetdb']
-#    Package['puppetdb'] -> File['puppetdb: database.ini']
+    #Set the defaults
+    Ini_setting {
+        path    => "${confdir}/database.ini",
+        require => File['{confdir}/database.ini'],
+        notifty => Service['puppetdb'],
+    }
 
+    if $database == 'embedded'{
+        $classname = 'org.hsqldb.jdbcDriver'
+        $subprotocol = 'hsqldb'
+        $subname = 'file:/usr/share/puppetdb/db/db;hsqldb.tx=mvcc;sql.syntax_pgs=true'
+    } elsif $database == 'postgres' {
+        $classname = 'org.postgresql.Driver'
+        $subprotocol = 'postgresql'
+        $subname = "//#${psdatabase_host}:${psdatabase_port}/${psdatabase}"
+    
+        ##Only setup for postgres
+        ini_setting {'puppetdb_psdatabase_username':
+            ensure  => present,
+            section => 'database',
+            setting => 'username',
+            value   => $psdatabase_username,
+        }
+
+        ini_setting {'puppetdb_psdatabase_username':
+            ensure  => present,
+            section => 'database',
+            setting => 'password',
+            value   => $psdatabase_password,
+        }
+    }
+    
+    ini_setting {'puppetdb_classname':
+            ensure  => present,
+            section => 'database',
+            setting => 'classname',
+            value   => $classname,
+    }
+
+    ini_setting {'puppetdb_subprotocol':
+            ensure  => present,
+            section => 'database',
+            setting => 'subprotocol',
+            value   => $subprotocol,
+    }
+
+    ini_setting {'puppetdb_pgs':
+            ensure  => present,
+            section => 'database',
+            setting => 'syntax_pgs',
+            value   => true,
+    }
+
+    ini_setting {'puppetdb_subname':
+            ensure  => present,
+            section => 'database',
+            setting => 'subname',
+            value   => $subname,
+    }
+
+    ini_setting {'puppetdb_gc_interval':
+            ensure  => present,
+            section => 'database',
+            setting => 'gc-interval ',
+            value   => $gc_interval ,
+    }
+    
 }
