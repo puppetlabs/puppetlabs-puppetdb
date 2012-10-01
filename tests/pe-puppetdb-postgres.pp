a598edc56a4d3ec423395d7b8fceff290335dd32
Hunter Haugen <h.haugen@gmail.com>
2012-10-01 10:58:22 -0700

    Add example manifest code for pe puppet master

--- tests/pe-puppetdb-postgres.pp
+++ tests/pe-puppetdb-postgres.pp
@@ -1,7 +1,17 @@
 # This manifest shows an example of how you might set up puppetdb to work with
-# Puppet Enterprise, as opposed to puppet open source.
+# a Puppet Enterprise environment consisting of a puppet master and a puppetdb
+# server, as opposed to puppet open source.
 
-class pe_puppetdb {
+node 'puppetmaster.example.com' {
+  class { 'puppetdb::master::config':
+    puppetdb_server     => 'puppetdb.example.com',
+    puppet_confdir      => '/etc/puppetlabs/puppet',
+    terminus_package    => 'pe-puppetdb-terminus',
+    puppet_service_name => 'pe-httpd',
+  }
+}
+
+node 'puppetdb.example.com' {
   class { 'puppetdb':
     puppetdb_package => 'pe-puppetdb',
     puppetdb_service => 'pe-puppetdb',
