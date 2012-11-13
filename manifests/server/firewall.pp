57445ef70fa52af3f06804320caba04f2a73f959
Drew Blessing <Drew.Blessing@Buckle.com>
2012-11-13 14:38:38 -0600

    17594 - PuppetDB - Add ability to set standard host listen address and open firewall to standard port
    Prior to this commit the module did not provide a way to set a bind address for the HTTP port.  This
    commit allows users to not only bind to an address and port other than localhost and 8080, but it also
    opens the firewall if explicitly requested.

--- manifests/server/firewall.pp
+++ manifests/server/firewall.pp
@@ -1,11 +1,23 @@
 class puppetdb::server::firewall(
-    $port                   = $puppetdb::params::ssl_listen_port,
+    $port                   = '',
+    $http_port              = $puppetdb::params::listen_port,             
+    $open_http_port         = $puppetdb::params::open_listen_port,
+    $ssl_port               = $puppetdb::params::ssl_listen_port,
+    $open_ssl_port          = $puppetdb::params::open_ssl_listen_port,
     $manage_redhat_firewall = $puppetdb::params::manage_redhat_firewall,
 ) inherits puppetdb::params {
   # TODO: figure out a way to make this not platform-specific; debian and ubuntu
   # have an out-of-the-box firewall configuration that seems trickier to manage.
   # TODO: the firewall module should be able to handle this itself
-  if ($manage_redhat_firewall and $puppetdb::params::firewall_supported) {
+  if ($puppetdb::params::firewall_supported) {
+
+    if ($manage_redhat_firewall) {
+      notify {'Deprecation notice: `$manage_redhat_firewall` is deprecated in the `puppetdb::service::firewall` class and will be removed in a future version. Use `open_http_port` and `open_ssl_port` instead.':}
+
+      if ($open_ssl_port) {
+        fail('`$manage_redhat_firewall` and `$open_ssl_port` cannot both be specified.')
+      }
+    }
 
     exec { 'puppetdb-persist-firewall':
       command     => $puppetdb::params::persist_firewall_command,
@@ -15,11 +27,29 @@ class puppetdb::server::firewall(
     Firewall {
       notify => Exec['puppetdb-persist-firewall']
     }
+    
+    if ($port) {
+      notify { 'Deprecation notice: `port` parameter will be removed in future versions of the puppetdb module. Please use ssl_port instead.': }
+    }
 
-    firewall { "${port} accept - puppetdb":
-      port   => $port,
-      proto  => 'tcp',
-      action => 'accept',
+    if ($port and $ssl_port) {
+      fail('`port` and `ssl_port` cannot both be defined. `port` is deprecated in favor of `ssl_port`')
+    }
+    
+    if ($open_http_port) {
+      firewall { "${http_port} accept - puppetdb":
+        port   => $http_port,
+        proto  => 'tcp',
+        action => 'accept',
+      }
+    } 
+
+    if ($open_ssl_port or $manage_redhat_firewall) {
+      firewall { "${ssl_port} accept - puppetdb":
+        port   => $ssl_port,
+        proto  => 'tcp',
+        action => 'accept',
+      }
     }
   }
 }
