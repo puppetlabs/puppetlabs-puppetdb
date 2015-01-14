1fe0a760325da327a8fc177576137aaf4ba6ac78
Tim Meusel <tim@bastelfreak.de>
2015-01-14 11:15:16 +0100

    add ability to manage postgres repo
    the module triggers puppetlabs-postgresql which is able to handle yum/apt repos from the postgres devs. Usefull in cases that your operating system doesn't provide the newest release. this feature is disabled by default
    
    fix typo

--- manifests/database/postgresql.pp
+++ manifests/database/postgresql.pp
@@ -1,14 +1,23 @@
 # Class for creating the PuppetDB postgresql database. See README.md for more
 # information.
 class puppetdb::database::postgresql(
-  $listen_addresses  = $puppetdb::params::database_host,
-  $database_name     = $puppetdb::params::database_name,
-  $database_username = $puppetdb::params::database_username,
-  $database_password = $puppetdb::params::database_password,
-  $manage_server     = $puppetdb::params::manage_dbserver,
+  $listen_addresses     = $puppetdb::params::database_host,
+  $database_name        = $puppetdb::params::database_name,
+  $database_username    = $puppetdb::params::database_username,
+  $database_password    = $puppetdb::params::database_password,
+  $manage_server        = $puppetdb::params::manage_dbserver,
+  $manage_package_repo  = $puppetdb::params::manage_pg_repo,
+  $postgres_version     = $puppetdb::params::postgres_version,
 ) inherits puppetdb::params {
 
-  if $manage_server == true {
+  if $manage_server {
+    if $manage_package_repo {
+      class { '::postgresql::globals':
+        manage_package_repo => true,
+        version             => $postgres_version,
+        before              => Class['::postgresql::server'],
+      }
+    }
     # get the pg server up and running
     class { '::postgresql::server':
       ip_mask_allow_all_users => '0.0.0.0/0',
