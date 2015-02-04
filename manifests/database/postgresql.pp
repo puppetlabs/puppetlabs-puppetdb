bc03812999ba78949a1ab2b7cb60fc1c4956f01d
Rick Fletcher <fletch@pobox.com>
2015-02-04 17:26:17 -0500

    Allow db management to be disabled
    Useful when PuppetDB is being configured to connect to an existing database.

--- manifests/database/postgresql.pp
+++ manifests/database/postgresql.pp
@@ -5,6 +5,7 @@ class puppetdb::database::postgresql(
   $database_name        = $puppetdb::params::database_name,
   $database_username    = $puppetdb::params::database_username,
   $database_password    = $puppetdb::params::database_password,
+  $manage_db            = $puppetdb::params::manage_db,
   $manage_server        = $puppetdb::params::manage_dbserver,
   $manage_package_repo  = $puppetdb::params::manage_pg_repo,
   $postgres_version     = $puppetdb::params::postgres_version,
@@ -24,10 +25,12 @@ class puppetdb::database::postgresql(
     }
   }
 
-  # create the puppetdb database
-  postgresql::server::db { $database_name:
-    user     => $database_username,
-    password => $database_password,
-    grant    => 'all',
+  if $manage_db {
+    # create the puppetdb database
+    postgresql::server::db { $database_name:
+      user     => $database_username,
+      password => $database_password,
+      grant    => 'all',
+    }
   }
 }
