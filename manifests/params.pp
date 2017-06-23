bfcf1f5130d4e4a3161306411a76b349c2966efc
Jan-Otto Kröpke <kroepke@outlook.com>
2017-06-23 14:47:33 +0200

    Fixed compatibility w/ puppetlabs-postgresql 5.0.0

--- manifests/params.pp
+++ manifests/params.pp
@@ -22,7 +22,7 @@ class puppetdb::params inherits puppetdb::globals {
 
   # The remaining database settings are not used for an embedded database
   $database_host          = 'localhost'
-  $database_port          = '5432'
+  $database_port          = 5432
   $database_name          = 'puppetdb'
   $database_username      = 'puppetdb'
   $database_password      = 'puppetdb'
@@ -48,7 +48,7 @@ class puppetdb::params inherits puppetdb::globals {
   # These settings are for the read database
   $read_database                     = 'postgres'
   $read_database_host                = undef
-  $read_database_port                = '5432'
+  $read_database_port                = 5432
   $read_database_name                = 'puppetdb'
   $read_database_username            = 'puppetdb'
   $read_database_password            = 'puppetdb'
