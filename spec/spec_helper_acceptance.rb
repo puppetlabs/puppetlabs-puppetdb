ee58d634153cbe0b57298ecfd86b5545eac01d16
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-06-30 21:24:52 +0200

    Remove ntp from acceptance testing

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -32,7 +32,6 @@ run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
 install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}i
 install_module_on(hosts)
 install_module_dependencies_on(hosts)
-install_module_from_forge_on(hosts, 'puppetlabs/ntp', '>= 5.0.0 < 7.0.0')
 
 RSpec.configure do |c|
   # Readable test descriptions
