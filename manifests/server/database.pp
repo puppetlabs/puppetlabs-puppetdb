3eb3849a2387a6f35f32acdd595f8f30c8f9af69
cocker-cc <github.com@cocker.cc>
2021-06-29 21:22:49 +0200

    Use Datatype Sensitive for Secrets

--- manifests/server/database.pp
+++ manifests/server/database.pp
@@ -5,7 +5,7 @@ class puppetdb::server::database (
   $database_host             = $puppetdb::params::database_host,
   $database_port             = $puppetdb::params::database_port,
   $database_username         = $puppetdb::params::database_username,
-  $database_password         = $puppetdb::params::database_password,
+  Variant[String[1], Sensitive[String[1]]] $database_password = $puppetdb::params::database_password,
   $database_name             = $puppetdb::params::database_name,
   $manage_db_password        = $puppetdb::params::manage_db_password,
   $jdbc_ssl_properties       = $puppetdb::params::jdbc_ssl_properties,
--- manifests/server/validate_db.pp
+++ manifests/server/validate_db.pp
@@ -5,7 +5,7 @@ class puppetdb::server::validate_db (
   $database_host       = $puppetdb::params::database_host,
   $database_port       = $puppetdb::params::database_port,
   $database_username   = $puppetdb::params::database_username,
-  $database_password   = $puppetdb::params::database_password,
+  Variant[String[1], Sensitive[String[1]]] $database_password = $puppetdb::params::database_password,
   $database_name       = $puppetdb::params::database_name,
   $jdbc_ssl_properties = $puppetdb::params::jdbc_ssl_properties,
 ) inherits puppetdb::params {
