bac44f7936b1868f52057368be24363c97dd6815
Robert Waffen <rwaffen@gmail.com>
2024-06-14 15:33:16 +0200

    Extend fact to handle debian packages too
    Signed-off-by: Robert Waffen <rwaffen@gmail.com>

--- lib/facter/puppetdb_version.rb
+++ lib/facter/puppetdb_version.rb
@@ -2,7 +2,26 @@ Facter.add(:puppetdb_version) do
   confine { Facter::Util::Resolution.which('puppetdb') }
 
   setcode do
-    output = Facter::Core::Execution.execute('puppetdb --version')
-    output.split(':').last.strip
+    require 'open3'
+
+    # check if os is debian/ubuntu and the package is not from puppetlabs
+    if Facter.value(:osfamily) == 'Debian'
+      package_maintainer = Facter::Core::Execution.execute('apt-cache show puppetdb | grep "Maintainer:" | head -1')
+      unless package_maintainer.include? 'Puppet Labs'
+        output, status = Open3.capture2('dpkg-query --showformat=\'${Version}\' --show puppetdb')
+        if status.success?
+          output.strip.split('-').first
+        else
+          nil
+        end
+      end
+    else
+      output, status = Open3.capture2('puppetdb --version')
+      if status.success?
+        output.split(':').last.strip
+      else
+        nil
+      end
+    end
   end
 end
--- spec/unit/facter/puppetdb_version_spec.rb
+++ spec/unit/facter/puppetdb_version_spec.rb
@@ -10,11 +10,29 @@ describe 'puppetdb_version' do
     Facter.clear
   end
 
-  it 'returns the correct puppetdb version' do
+  it 'returns a version on non-Debian family with puppetlabs package' do
     allow(Facter::Util::Resolution).to receive(:which).with('puppetdb').and_return('/usr/bin/puppetdb')
-    allow(Facter::Core::Execution).to receive(:execute).with('puppetdb --version').and_return("puppetdb version: 7.18.0\n")
+    allow(Open3).to receive(:capture2).with('puppetdb --version').and_return("puppetdb version: 7.18.0\n")
 
     expect(Facter.fact(:puppetdb_version).value).to eq('7.18.0')
+  end
+
+  it 'returns a version on Debian family with non-puppetlabs package' do
+    allow(Facter::Util::Resolution).to receive(:which).with('puppetdb').and_return('/usr/sbin/puppetdb')
+    allow(Facter).to receive(:value).with(:osfamily).and_return('Debian')
+    allow(Facter::Core::Execution).to receive(:execute).with('apt-cache show puppetdb | grep "Maintainer:" | head -1').and_return('Maintainer: Ubuntu Developers')
+    allow(Open3).to receive(:capture2).with('dpkg-query --showformat=\'${Version}\' --show puppetdb').and_return("6.2.0-5")
+
+    expect(Facter.fact(:puppetdb_version).value).to eq('6.2.0')
+  end
+
+  it 'returns a version on Debian family with puppetlabs package' do
+    allow(Facter::Util::Resolution).to receive(:which).with('puppetdb').and_return('/usr/sbin/puppetdb')
+    allow(Facter).to receive(:value).with(:osfamily).and_return('Debian')
+    allow(Facter::Core::Execution).to receive(:execute).with('apt-cache show puppetdb | grep "Maintainer:" | head -1').and_return('Maintainer: Puppet Labs')
+    allow(Open3).to receive(:capture2).with('dpkg-query --showformat=\'${Version}\' --show puppetdb').and_return("7.19.0-1jammy")
+
+    expect(Facter.fact(:puppetdb_version).value).to eq('7.19.0')
   end
 
   it 'returns nil if puppetdb command is not available' do
