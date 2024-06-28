a535c4d8996aba86db98de458394d2fd60e16b64
Robert Waffen <rwaffen@gmail.com>
2024-06-28 10:34:55 +0200

    Drop work on also handling debian package.
    - just report nil if command fails
    
    Signed-off-by: Robert Waffen <rwaffen@gmail.com>

--- lib/facter/puppetdb_version.rb
+++ lib/facter/puppetdb_version.rb
@@ -2,25 +2,9 @@ Facter.add(:puppetdb_version) do
   confine { Facter::Util::Resolution.which('puppetdb') }
 
   setcode do
-    command = 'puppetdb --version'
-    splitter = ':'
-    postion = 'last'
-
-    if Facter.value(:os)['family'] == 'Debian'
-      package_maintainer = Facter::Core::Execution.execute('apt-cache show puppetdb | grep "Maintainer:" | head -1')
-
-      unless package_maintainer.include? 'Puppet Labs'
-        command = 'dpkg-query --showformat=\'${Version}\' --show puppetdb'
-        splitter = '-'
-        postion = 'first'
-      end
-    end
-
-    begin
-      output = Facter::Core::Execution.execute(command)
-      output.split(splitter).send(postion).strip
-    rescue Facter::Core::Execution::ExecutionFailure
-      nil
-    end
+    output = Facter::Core::Execution.execute('puppetdb --version')
+    output.split(':').last.strip
+  rescue Facter::Core::Execution::ExecutionFailure
+    nil
   end
 end
--- spec/unit/facter/puppetdb_version_spec.rb
+++ spec/unit/facter/puppetdb_version_spec.rb
@@ -6,65 +6,11 @@ describe 'puppetdb_version' do
   end
 
   context 'when puppetdb is available' do
-    before do
+    before(:each) do
       allow(Facter::Util::Resolution).to receive(:which).with('puppetdb').and_return('/usr/bin/puppetdb')
     end
 
-    context 'on a Debian-based system' do
-      before do
-        allow(Facter).to receive(:value).with(:os).and_return({ 'family' => 'Debian' })
-      end
-
-      context 'when Puppet Labs is the maintainer' do
-        before do
-          allow(Facter::Core::Execution).to receive(:execute)
-            .with('apt-cache show puppetdb | grep "Maintainer:" | head -1')
-            .and_return('Maintainer: Puppet Labs')
-        end
-
-        it 'returns the correct version from puppetdb --version' do
-          expect(Facter::Core::Execution).to receive(:execute)
-            .with('puppetdb --version')
-            .and_return('puppetdb version: 7.19.0')
-
-          expect(Facter.fact(:puppetdb_version).value).to eq('7.19.0')
-        end
-
-        it 'returns nil if the command execution fails' do
-          allow(Facter::Core::Execution).to receive(:execute).with('puppetdb --version').and_raise(Facter::Core::Execution::ExecutionFailure)
-
-          expect(Facter.fact(:puppetdb_version).value).to be_nil
-        end
-      end
-
-      context 'when Puppet Labs is not the maintainer' do
-        before do
-          allow(Facter::Core::Execution).to receive(:execute)
-            .with('apt-cache show puppetdb | grep "Maintainer:" | head -1')
-            .and_return('Maintainer: Other Maintainer')
-        end
-
-        it 'returns the correct version from dpkg-query' do
-          expect(Facter::Core::Execution).to receive(:execute)
-            .with("dpkg-query --showformat='${Version}' --show puppetdb")
-            .and_return('7.9.0-1ubuntu1')
-
-          expect(Facter.fact(:puppetdb_version).value).to eq('7.9.0')
-        end
-
-        it 'returns nil if the command execution fails' do
-          allow(Facter::Core::Execution).to receive(:execute).with("dpkg-query --showformat='${Version}' --show puppetdb").and_raise(Facter::Core::Execution::ExecutionFailure)
-
-          expect(Facter.fact(:puppetdb_version).value).to be_nil
-        end
-      end
-    end
-
-    context 'on a non-Debian-based system' do
-      before do
-        allow(Facter).to receive(:value).with(:os).and_return({ 'family' => 'RedHat' })
-      end
-
+    context 'on a default system' do
       it 'returns the correct version from puppetdb --version' do
         expect(Facter::Core::Execution).to receive(:execute)
           .with('puppetdb --version')
@@ -82,7 +28,7 @@ describe 'puppetdb_version' do
   end
 
   context 'when puppetdb is not available' do
-    before do
+    before(:each) do
       allow(Facter::Util::Resolution).to receive(:which).with('puppetdb').and_return(nil)
     end
 
